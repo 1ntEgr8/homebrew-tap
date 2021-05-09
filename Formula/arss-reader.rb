ca9c52b376e5b01eb754d668ecf195d6496b0e6b
Elton <eltonp3103@gmail.com>
2021-05-08 23:39:02 -0400

    bump arss

--- Formula/arss-reader.rb
+++ Formula/arss-reader.rb
@@ -3,8 +3,8 @@ require "language/node"
 class ArssReader < Formula
   desc "Simple rss reader"
   homepage "https://github.com/1ntEgr8/arss"
-  url "https://github.com/1ntEgr8/arss/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "48dd2fb90ede37e5688568e0c4bc0a7cc47e204535fdae9eb3b434aefe820a16"
+  url "https://github.com/1ntEgr8/arss/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "4bf113abc4df69e4c29418a5659b1e57d3e04b7b16dc944b2fa5ef54647f44fc"
   license "MIT"
 
   depends_on "go" => :build
